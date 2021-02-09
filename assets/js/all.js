6c611a0c9649f250107b412b1d15eb51e5a7971b
Dima Vlasenko <dima@Dimas-MacBook-Air.local>
2021-02-10 01:28:43 +0200

    header

---
+++ assets/js/all.js
@@ -0,0 +1,28 @@
+console.log('1');
+console.log('2');
+console.log('3');
+console.log('4');
+console.log('5');
+
+console.log('new');
+console.log('new');
+console.log('new');
+console.log('new');
+console.log('new');
+
+/* eslint-disable multiline-comment-style */
+/* eslint-disable no-var */
+/* eslint-disable no-unused-vars */
+let num = 10;
+
+console.log('OUTPUT: num', num + 1);
+let str = 'str';
+
+console.log('OUTPUT: str', str + 2);
+
+let func = () => {
+  console.log('func');
+  console.log('func2');
+  num = 100;
+  str = `${num}`;
+};
---
+++ assets/js/new.js
@@ -0,0 +1,5 @@
+console.log('new');
+console.log('new');
+console.log('new');
+console.log('new');
+console.log('new');
