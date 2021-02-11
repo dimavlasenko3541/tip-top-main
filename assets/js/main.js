388f5df47c6eefb9fd2cd73baa60af114a03f9e9
Dima Vlasenko <dima@Dimas-MacBook-Air.local>
2021-02-12 01:37:52 +0200

    about

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,20 @@
-console.log('1');
-console.log('2');
-console.log('3');
-console.log('4');
-console.log('5');
+$(document).ready(function () {
+    $('.cards-slider').slick({
+        arrows: true,
+        rows: 2,
+        adaptiveHeight: true,
+        slidesToShow: 3,
+        speed: 1000,
+        draggable: false,
+        toushTreshold: 10,
+        waitForAnimate: false,
+        responsive: [
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 2,
+                }
+            }
+        ]
+    });
+});
